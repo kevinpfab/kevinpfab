3567019d0196c407fbcf24e2a9fd5e949302ef84
Kevin Pfab <kevin.pfab@gmail.com>
2011-10-17 20:31:33 -0400

    Design improving. Added home page image, and url helper function.

--- controller.rb
+++ controller.rb
@@ -1,16 +1,26 @@
 # Statis Controller
 
+helpers do
+    def kp(s = "")
+        "http://kevinpfab.com/#{s}"
+    end
+end
+
 # Navigation
 # Execute before each page render
 before /.*html\.haml/ do
+    @site = "http://kevinpfab.com/"
     @nav = [
         {
-            :url => "/",
+            :url => kp,
             :name => "Home",
         },
         {
-            :url => "/essays.html",
+            :url => kp("essays.html"),
             :name => "Essays",
+        },
+        {
+            :name => "Links",
         },
         {
             :url => "http://github.com/kevinpfab",
